c6bc8c6f5fd61a294cb32296178fbe1c3707c352
ronnie-1205 <ronniescratcher@gmail.com>
2026-09-12 01:47:50 +0530

    Commit 2.4: More random fixes.

--- test_db.py
+++
@@ -1,5 +0,0 @@
-from backend.database import SessionLocal, Inventory
-db = SessionLocal()
-inv = db.query(Inventory).first()
-print(type(inv.consumption_history))
-print(inv.consumption_history)
--- test_fetch.js
+++
@@ -1,9 +0,0 @@
-fetch('http://127.0.0.1:8001/api/simulate', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ days: 15 })
-}).then(r => r.json()).then(data => {
-  console.log("Timeline days:", data.days.length);
-  console.log("Hospitals day 0:", data.days[0].hospitals.length);
-  console.log("First hospital:", data.days[0].hospitals[0]);
-}).catch(console.error);
